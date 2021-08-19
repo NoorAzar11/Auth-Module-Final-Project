59f614ecc9982499194d76ecd0a07d80865a6c4b
NoorAzar11 <nooraaz945@gamil.com>
2021-08-19 13:24:00 +0300

    update

--- models/index.js
+++ models/index.js
@@ -5,10 +5,18 @@ const gamesModels = require('./games/model');
 const userModel = require('./users');
 const Collection = require('./data-collection');
 
-const DATABASE_URL = process.env.DATABASE_URL;
-let sequelizeOptions = { dialectOptions: { ssl: { require: true, rejectUnauthorized: false, } } };
-const sequelize = new Sequelize(DATABASE_URL , sequelizeOptions);
+
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL||'postgres://localhost:5432/noor';
+// const DATABASE_URL = process.env.DATABASE_URL 
+const sequelize = new Sequelize(DATABASE_URL);
+// const DATABASE_URL = process.env.DATABASE_URL;
+// let sequelizeOptions = { dialectOptions: { ssl: { require: true, rejectUnauthorized: false, } } };
+// const sequelize = new Sequelize(DATABASE_URL , sequelizeOptions);
+
 const games = gamesModels(sequelize, DataTypes);
+
+
 
 module.exports = {
   db: sequelize,
